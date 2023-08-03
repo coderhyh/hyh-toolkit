cc254df621c94a6f5431b8b51647282453015169
coderhyh <772567615@qq.com>
2023-08-03 10:41:03 +0800

    feat: delete a file

--- .prod.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs'
-
-fs.readFile('./cli/lib/index.js', { encoding: 'utf-8' }, (err, data) => {
-  if (err) return console.error(err)
-
-  fs.writeFile('./cli/lib/index.js', data.replace('ts-node', 'node'), (err) => {
-    err && console.error(err)
-  })
-})
