8b16a0f915194ddc8d6f37662e3a60594ad7f6c0
coderhyh <772567615@qq.com>
2024-12-02 16:16:07 +0800

    feat(custom): update type

--- packages/vite/pinia-auto-refs/index.ts
+++ packages/vite/pinia-auto-refs/index.ts
@@ -49,10 +49,10 @@ type IsUnion<T> = [T] extends [UnionToIntersection<T>] ? false : true
 type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (k: infer I) => void ? I : never
 
 type AutoToRefs<T> = {
-  [K in keyof T]: T[K] extends Function ? T[K] : K extends \`$\${infer P}\` ? P : ToRef<T[K]>
+  [K in keyof T]: T[K] extends (...args: any[]) => any ? T[K] : K extends \`$\${infer P}\` ? P : ToRef<T[K]>
 }
 type PartialRef<T extends Record<string, any>> = {
-  [K in keyof T]: T[K] extends Function ? T[K] : K extends \`$\${infer P}\` ? P : ToRef<T[K]['value'] | undefined>
+  [K in keyof T]: T[K] extends (...args: any[]) => any ? T[K] : K extends \`$\${infer P}\` ? P : ToRef<T[K]['value'] | undefined>
 }
 
 const storeExports = {
